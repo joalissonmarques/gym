d02e09f80bf741a6142dc170bb6c1403dfbd146f
joalissonmarques <joalisonmarques@hotmail.com>
2025-08-14 14:33:28 -0300

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import pluginRouter from "@tanstack/eslint-plugin-router";
+import eslintConfigPrettier from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import { dirname } from "path";
+import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +15,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+      ...pluginQuery.configs["flat/recommended"],
+      ...pluginRouter.configs["flat/recommended"],
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    rules: { ...reactHooks.configs.recommended.rules },
+  }),
 ];
 
 export default eslintConfig;
